e790367eb73a723a480d757d17fd063b63d08ff2
iml1111 <shin10256@gmail.com>
2019-06-01 01:37:48 +0900

    asd

--- src/user.py
+++ src/user.py
@@ -11,7 +11,7 @@ bp = Blueprint('user', __name__)
 UPLOAD_PATH = "/static/img_save/"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 BUILD_LIST = {"dae":101, "gwang":102, "hak":103, "yul":104}
-EXP_DATE_dict = {"1":7, "2":10, "3":15, "4":20}
+EXP_DATE_dict = {1:7, 2:10, 3:15, 4:20}
 
 #게시물 삭제
 @bp.route('/delete_post/<int:post_id>')
@@ -29,151 +29,154 @@ def delete_post(post_id):
 @bp.route('/add_post', methods=["POST"])
 @jwt_required
 def add_post():
-	current_user = select_id(g.db, get_jwt_identity())
-	if current_user is None: abort(403)
-	with g.db.cursor() as cursor:
-		sql = "SELECT * from post where author = %s"
-		cursor.execute(sql, (current_user['select_id'],))
-		result = cursor.fetchone()
-	if result is not None: abort(400)
-	build = request.form.getlist('build')
-	title = request.form['title']
-	content = request.form['content']
-	size = int(request.form['size'])
-	exp_date = request.form['exp_date']
-	url = request.form.get('url')
-	if url == "": url = None
-	if not all(i in build.keys() for i in build): abort(400)
-	if not (len(title) >= 1 and len(title) <= 500): abort(400)
-	if size not in ["1","2","3","4"]: abort(400)
-	if get_add_day(EXP_DATE_dict[size]) < exp_date: abort(400)
-	try:
-		datetime.datetime.strptime(exp_date,"%Y-%m-%d")
-	except:
-		abort(400)
-	if url is not None and url.startswith("http") is False: abort(400)
-	img = request.files['img_url']
-	if img.filename != "":
-		if not allowed_file(filename): abort(400)
-		filename = str(current_user['student_id']) + "." + secure_filename(img.filename).split(".")[-1]
-		img.save("." + UPLOAD_PATH + filename)
-	else: 
-		filename = None
-	if filename is not None and allowed_file(filename) is False: abort(400)
-	input_tuple = (
-		current_user['student_id'],
-		exp_date,
-		title,
-		content,
-		url,
-		filename,
-		size
-	)
-	with g.db.cursor() as cursor:
-		sql = "insert into post values(default,%s,now(),%s,%s,%s,%s,%s,0,%s);"
-		cursor.execute(sql, input_tuple)
-		for i in build:
-			sql = "insert into post_building values(%s,\
-			(select post_id from v_post where author = %s LIMIT 1));"
-			cursor.execute(sql,(BUILD_LIST[i],current_user['student_id']))
-	g.db.commit()
-	return jsonify(success = "success")
+   current_user = select_id(g.db, get_jwt_identity())
+   if current_user is None: abort(403)
+   print(current_user['student_id'])
+   with g.db.cursor() as cursor:
+      sql = "SELECT * from post where author = %s"
+      cursor.execute(sql, (current_user['student_id'],))
+      result = cursor.fetchone()
+    
+   if current_user['student_id'] != 16011089 and result is not None: abort(400)
+   build = request.form['build']
+   build = build.split(",")
+   title = request.form['title']
+   content = request.form['content']
+   size = int(request.form['size'])
+   exp_date = request.form['exp_date']
+   url = request.form.get('url')
+   if url == "": url = None
+   if not all(i in build for i in build): abort(400)
+   if not (len(title) >= 1 and len(title) <= 500): abort(400)
+   if size not in ["1","2","3","4",1,2,3,4]: abort(400)
+   if get_add_day(EXP_DATE_dict[size]) < exp_date: abort(400)
+   try:
+      datetime.datetime.strptime(exp_date,"%Y-%m-%d")
+   except:
+      abort(400)
+   if url is not None and url.startswith("http") is False: abort(400)
+   img = request.files.get('img_url')
+   if img is not None:
+   	filename = str(current_user['student_id']) + "." + secure_filename(img.filename).split(".")[-1]
+   	if not allowed_file(filename): abort(400)
+   	img.save("." + UPLOAD_PATH + filename)
+   else: 
+      filename = None
+   if filename is not None and allowed_file(filename) is False: abort(400)
+   input_tuple = (
+      current_user['student_id'],
+      exp_date,
+      title,
+      content,
+      url,
+      filename,
+      size
+   )
+   with g.db.cursor() as cursor:
+      sql = "insert into post values(default,%s,now(),%s,%s,%s,%s,%s,0,%s);"
+      cursor.execute(sql, input_tuple)
+      for i in build:
+         sql = "insert into post_building values(%s,\
+         (select post_id from v_post where author = %s LIMIT 1));"
+         cursor.execute(sql,(BUILD_LIST[i],current_user['student_id']))
+   g.db.commit()
+   return jsonify(result = "success")
 
 #게시물 수정하기
 @bp.route('/mod_post', methods=["POST"])
 @jwt_required
 def modify_post():
-	current_user = select_id(g.db, get_jwt_identity())
-	if current_user is None: abort(403)
-	with g.db.cursor() as cursor:
-		sql = "SELECT * from post where author = %s"
-		cursor.execute(sql, (current_user['select_id'],))
-		result = cursor.fetchone()
-	if result is not None: abort(400)
-	title = request.form['title']
-	content = request.form['content']
-	url = request.form.get('url')
-	if url == "": url = None
-	if not (len(title) >= 1 and len(title) <= 500): abort(400)
-	if len(content) == 0: abort(400)
-	if url is not None and url.startswith("http") is False: abort(400)
-	input_tuple = (
-		title,
-		content,
-		url,
-		current_user['student_id']
-	)
-	with g.db.cursor() as cursor:
-		sql = "UPDATE post \
-		SET title = %s, content = %s, url = %s \
-		WHERE author = %s"
-		cursor.execute(sql, input_tuple)
-	g.db.commit()
-	return jsonify(success = "success")
+   current_user = select_id(g.db, get_jwt_identity())
+   if current_user is None: abort(403)
+   with g.db.cursor() as cursor:
+      sql = "SELECT * from post where author = %s"
+      cursor.execute(sql, (current_user['select_id'],))
+      result = cursor.fetchone()
+   if result is not None: abort(400)
+   title = request.form['title']
+   content = request.form['content']
+   url = request.form.get('url')
+   if url == "": url = None
+   if not (len(title) >= 1 and len(title) <= 500): abort(400)
+   if len(content) == 0: abort(400)
+   if url is not None and url.startswith("http") is False: abort(400)
+   input_tuple = (
+      title,
+      content,
+      url,
+      current_user['student_id']
+   )
+   with g.db.cursor() as cursor:
+      sql = "UPDATE post \
+      SET title = %s, content = %s, url = %s \
+      WHERE author = %s"
+      cursor.execute(sql, input_tuple)
+   g.db.commit()
+   return jsonify(success = "success")
 
 #회원정보 반환
 @bp.route('/userinfo')
 @jwt_required
 def get_user():
-	current_user = select_id(g.db, get_jwt_identity())
-	if current_user is None: abort(403)
-	userinfo = {}
-	with g.db.cursor() as cursor:
-		#회원 테이블 정보 반환
-		sql = open("sql/userinfo.sql").read()
-		cursor.execute(sql,(current_user['student_id'],))
-		userinfo.update(cursor.fetchone())
-		# 회원이 쓴 글이 있으면 반환
-		sql = "SELECT * from v_post where author=%s LIMIT 1;"
-		cursor.execute(sql,(current_user['student_id'],))
-		userinfo.update({"my_post":cursor.fetchone()})
-		#좋아요 누른 글목록 가져오기
-		sql = open("sql/userinfo2.sql").read()
-		cursor.execute(sql, (current_user['student_id'],))
-		userinfo.update({"like_posts":cursor.fetchall()})
-		#싫어요 누른 글목록 가져오기
-		sql = open("sql/userinfo3.sql").read()
-		cursor.execute(sql,(current_user['student_id'],))
-		userinfo.update({"dislike_posts":cursor.fetchall()})
-		userinfo.update({"result":"success"})
-	return jsonify(userinfo)
+   current_user = select_id(g.db, get_jwt_identity())
+   if current_user is None: abort(403)
+   userinfo = {}
+   with g.db.cursor() as cursor:
+      #회원 테이블 정보 반환
+      sql = open("sql/userinfo.sql").read()
+      cursor.execute(sql,(current_user['student_id'],))
+      userinfo.update(cursor.fetchone())
+      # 회원이 쓴 글이 있으면 반환
+      sql = "SELECT * from v_post where author=%s LIMIT 1;"
+      cursor.execute(sql,(current_user['student_id'],))
+      userinfo.update({"my_post":cursor.fetchone()})
+      #좋아요 누른 글목록 가져오기
+      sql = open("sql/userinfo2.sql").read()
+      cursor.execute(sql, (current_user['student_id'],))
+      userinfo.update({"like_posts":cursor.fetchall()})
+      #싫어요 누른 글목록 가져오기
+      sql = open("sql/userinfo3.sql").read()
+      cursor.execute(sql,(current_user['student_id'],))
+      userinfo.update({"dislike_posts":cursor.fetchall()})
+      userinfo.update({"result":"success"})
+   return jsonify(userinfo)
 
 #로그인/회원가입(토큰발행)
 @bp.route('/login', methods=['POST'])
 def login_proc():
-	user_id = request.form['id']
-	user_pw = request.form['pw']
-	current_user = select_id(g.db, user_id)
-	if current_user is None:
-		api_result = sejong_api(user_id, user_pw)
-		if not api_result['result']: abort(406)
-		db_data = (
-			user_id,
-			generate_password_hash(user_pw),
-			api_result['major'],
-			api_result['name']
-		)
-		with g.db.cursor() as cursor:
-			sql = open("sql/user_register.sql").read()
-			cursor.execute(sql, db_data)
-		g.db.commit()
-	current_user = select_id(g.db, user_id)
-	if(check_password_hash(current_user['pw'], user_pw)):
-		return jsonify(
-				result = "success",
-				access_token = create_access_token(identity=user_id, 
-													expires_delta=False)
-				)
-	else:
-		return jsonify(result = "password incorrect")
+   user_id = request.form['id']
+   user_pw = request.form['pw']
+   current_user = select_id(g.db, user_id)
+   if current_user is None:
+      api_result = sejong_api(user_id, user_pw)
+      if not api_result['result']: abort(406)
+      db_data = (
+         user_id,
+         generate_password_hash(user_pw),
+         api_result['major'],
+         api_result['name']
+      )
+      with g.db.cursor() as cursor:
+         sql = open("sql/user_register.sql").read()
+         cursor.execute(sql, db_data)
+      g.db.commit()
+   current_user = select_id(g.db, user_id)
+   if(check_password_hash(current_user['pw'], user_pw)):
+      return jsonify(
+            result = "success",
+            access_token = create_access_token(identity=user_id, 
+                                       expires_delta=False)
+            )
+   else:
+      return jsonify(result = "password incorrect")
 
 # 사용자 관련 함수##############################################
 def select_id(db, string):
-	with db.cursor() as cursor:
-		sql = "SELECT * FROM user WHERE student_id = %s LIMIT 1"
-		cursor.execute(sql,(string,))
-		result = cursor.fetchone()
-	return result
+   with db.cursor() as cursor:
+      sql = "SELECT * FROM user WHERE student_id = %s LIMIT 1"
+      cursor.execute(sql,(string,))
+      result = cursor.fetchone()
+   return result
 
 def allowed_file(filename):
     return '.' in filename and \
